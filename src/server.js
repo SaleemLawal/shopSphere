6aafa54b35d28e0a7ca056efd0c39f006ff05123
saleemLawal <slawal1@umbc.edu>
2025-02-12 17:29:22 -0500

    Initial commit

---
+++ src/server.js
@@ -0,0 +1,18 @@
+import { app } from "./app.js";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const port = process.env.PORT || 3001;
+
+try {
+  await mongoose.connect(process.env.MONGODB_URI);
+  console.log("Connected to DB");
+} catch (error) {
+  console.error(error);
+}
+
+app.listen(port, () => {
+  console.log("Server is running on port 3000");
+});
